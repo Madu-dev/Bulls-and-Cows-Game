3d8255a234aa0ab35cd8f80d5aabc2a9818e1141
Maria <silvestre.mca@gmail.com>
2022-10-12 00:31:58 -0300

    verificando os números

--- scripts.js
+++ scripts.js
@@ -8,3 +8,42 @@ const secondTable = document.querySelectorAll(".second");
 let count = 0;
 let randomArray = new Array();
 let valueArray = new Array();
+
+const randomValue = () => {
+    randomArray = [];
+    while (randomArray.length < 4) {
+      const randomNumber = Math.floor(Math.random() * 10);
+      if (randomArray.indexOf(randomNumber) < 0) {
+        randomArray.push(randomNumber);
+      }
+    }
+  };
+  
+  const inputValue = () => {
+    valueArray = [];
+    input.forEach((text) => {
+      valueArray.push(parseInt(text.value));
+    });
+  
+    const blankCheck = valueArray.indexOf("") !== -1;
+    const nanCheck = valueArray.includes(NaN) === true;
+    const valueRangeCheck = valueArray.find((item) => {
+      return item >= 10;
+    });
+    const duplicateNumber = new Set(valueArray);
+
+    //verificando se tem número, se tá duplicado ou se é maior que o permitido
+  if (blankCheck || nanCheck) {
+    errorText.innerText = "Por favor, coloque um numero.";
+  } else if (valueRangeCheck) {
+    errorText.innerText = "Insira um número menor que 10.";
+  } else if (duplicateNumber.size < 4) {
+    errorText.innerText = "Existem números duplicados.";
+  } else {
+    errorText.innerText = "";
+    firstTable[count].innerText = valueArray;
+    vacaAndtouro();
+    count += 1;
+  }
+  result();
+};
