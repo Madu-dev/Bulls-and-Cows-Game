1f3de7ae4da45ac7f3cf3bbc1e2a2839502a1645
Maria <silvestre.mca@gmail.com>
2022-10-12 00:29:17 -0300

    index alterado

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const input = document.querySelectorAll("input");
+const buttons = document.querySelectorAll("button");
+const gameResult = document.querySelector(".gameResult");
+const errorText = document.querySelector(".errorText");
+const firstTable = document.querySelectorAll(".first");
+const secondTable = document.querySelectorAll(".second");
+
+let count = 0;
+let randomArray = new Array();
+let valueArray = new Array();
