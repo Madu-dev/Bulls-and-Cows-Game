e32b69546dcaf8bb5dc0ec7f48d14eedebefe242
Maria do Carmo <69973218+Madu-dev@users.noreply.github.com>
2022-11-04 10:07:34 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const input = document.querySelectorAll("input"); 
 const buttons = document.querySelectorAll("button"); 
-const gameResult = document.querySelector(".gameResult");
+const gameResult = document.querySelector(".gameResult"); 
 const errorText = document.querySelector(".errorText");
 const firstTable = document.querySelectorAll(".first");
 const secondTable = document.querySelectorAll(".second");
