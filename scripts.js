f6f4d446f6a33ca9a0bedd26758b476e7eff0900
Maria do Carmo <69973218+Madu-dev@users.noreply.github.com>
2022-11-04 10:08:00 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const buttons = document.querySelectorAll("button");
 const gameResult = document.querySelector(".gameResult"); 
 const errorText = document.querySelector(".errorText"); 
 const firstTable = document.querySelectorAll(".first");
-const secondTable = document.querySelectorAll(".second");
+const secondTable = document.querySelectorAll(".second"); 
 
 let count = 0;
 let randomArray = new Array();
